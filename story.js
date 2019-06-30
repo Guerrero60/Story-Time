325ad6299d52fb44d1b8981f6b390b0ed06bdec7
Guerrero60 <47193887+Guerrero60@users.noreply.github.com>
2019-06-30 13:18:34 -0700

    Add files via upload

---
+++ story.js
@@ -0,0 +1,8 @@
+var adjective = prompt("Please input an adjective");
+var sentence ="<h2> There was once a programmer that was " + adjective;
+var verb = prompt("Please insert verb");
+var noun = prompt("Please input noun");
+alert("Thank you for creating a story enjoy.");
+sentence += " who believed they could help by " + verb;
+sentence += " so they proceeded to " + noun + ".</h2>";
+document.write(sentence); 
