991d2bd7e01cc059fd6c46a8db9ee7ada07b5caf
Guerrero60 <47193887+Guerrero60@users.noreply.github.com>
2019-12-18 11:12:37 -0800

    Update story.js

--- story.js
+++ story.js
@@ -3,6 +3,6 @@ var sentence ="<h2> There was once a programmer that was " + adjective;
 var verb = prompt("Please insert verb");
 var noun = prompt("Please input noun");
 alert("Thank you for creating a story enjoy.");
-sentence += " who believed they could help by " + verb;
+sentence += " who believed they could help spread knowledge by " + verb;
 sentence += " so they proceeded to " + noun + ".</h2>";
 document.write(sentence); 
